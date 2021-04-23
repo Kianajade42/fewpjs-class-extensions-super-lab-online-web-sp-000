4fc91a64c491acf4cf5d9d8bf20e4c6ff4586c74
Kiana Long <kianajade42.kl@gmail.com>
2021-04-23 23:11:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ class Tree{
   static definition(){
     return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves'
   }
+}
    class Deciduous extends Tree {
      constructor(species,name){
        super(species)
